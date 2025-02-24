7dbd62f48eac7d130d12eaaa5ecb3367f6f99981
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-02-24 18:28:16 +0900

    :sparkles: feat: 커스텀 사이드바 구현 및 레이아웃 적용

--- src/app/playlists/layout.tsx
+++ src/app/playlists/layout.tsx
@@ -1,8 +1,18 @@
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import AppSidebar from '@/components/playlists/AppSidebar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen">
-      <aside>{/* TODO: Sidebar & MobileNav */}</aside>
-      <main className="flex-1">{children}</main>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <main className="flex-1">
+        <header className="flex h-16 items-center">
+          <div className="px-4">
+            <SidebarTrigger />
+          </div>
+        </header>
+        <div className="px-4">{children}</div>
+      </main>
+    </SidebarProvider>
   );
 }
