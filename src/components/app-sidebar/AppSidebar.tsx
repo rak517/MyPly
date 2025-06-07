bae56ca1b354c5a5a413136c9ae35586f1436f0f
chltjdfkr517@naver.com <chltjdfkr517@naver.com>
2025-06-07 22:15:02 +0900

    :sparkles: feat: 사이드바 메뉴 상수 정의 및 네비게이션 스타일링

--- src/components/app-sidebar/AppSidebar.tsx
+++ src/components/app-sidebar/AppSidebar.tsx
@@ -1,12 +1,32 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from '../ui/sidebar';
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import Logo from '../ui/Logo';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '../ui/sidebar';
+import { navItems } from '@/constants/navigation';
 
 export default function AppSidebar() {
+  const pathname = usePathname();
+
   return (
     <Sidebar>
-      <SidebarHeader />
+      <SidebarHeader className='p-5'>
+        <Logo />
+      </SidebarHeader>
       <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
+        <SidebarMenu className='px-4'>
+          {navItems.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <SidebarMenuButton asChild className='h-14 text-lg' isActive={pathname === item.url}>
+                <Link href={item.url} className='flex items-center gap-4 pl-2'>
+                  <item.icon className='size-7' />
+                  <span className='font-medium'>{item.title}</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
       </SidebarContent>
       <SidebarFooter />
     </Sidebar>
